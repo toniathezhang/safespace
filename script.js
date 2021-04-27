a8fe21c0e2c113c5e53f8c0e6f69992c63a8d7e0
NYUAD <nyuad@C2-LIB-MBP-19.local>
2021-04-27 17:53:53 +0400

    pushihao

---
+++ script.js
@@ -0,0 +1,10 @@
+let pressButton = document.getElementById("press")
+let text = document.getElementById("textbox")
+
+pressButton.addEventListener("click", showText)
+
+function showText() {
+
+alert("Yo!")
+
+}
